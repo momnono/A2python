6a54223c415ffd66f44bb50e15c874a6d9e947da
mo <alshmoha@shernet.sheridancollege.ca>
2023-03-10 23:36:34 -0500

    added the code for the methods and class

--- Product.py
+++ Product.py
@@ -1,10 +1,42 @@
 """attributes for product code, product name, sale price, manufacture cost, stock level, and estimated monthly units manufactured. It should also have methods for calculating the total units sold, the net profit, and for simulating the monthly production and sale of the product"""
+import random
+
 class Product:
-    def __init__(self, code, name, sale_price, manufacture_cost, stock_level, monthly_units_manufactured):
-       pass
-    def simulate_monthly_production_and_sales(self):
-        pass
+    def __init__(self, code, name, price, cost, stock):
+        self.code = code
+        self.name = name
+        self.price = price
+        self.cost = cost
+        self.stock = stock
         
+    def sell(self, qty):
+        if qty > self.stock:
+            print("Not enough stock!")
+            return 0
+        else:
+            self.stock -= qty
+            return self.price * qty
+    
+    def restock(self, qty):
+        self.stock += qty
         
-    def get_predicted_stock_statement(self):
-        pass
+    def predict_stock(self):
+        monthly_sold = random.randint(5, 15)
+        monthly_made = monthly_sold + random.randint(-5, 5)
+        net_profit = (self.price - self.cost) * monthly_sold
+        
+        report = f"Product Code: {self.code}\nProduct Name: {self.name}\nCurrent Stock: {self.stock}\n"
+        report += "Predicted Monthly Stock for the next 12 months:\n"
+        
+        for month in range(1, 13):
+            report += f"Month {month}: "
+            report += f"Units Sold: {monthly_sold}, "
+            report += f"Units Made: {monthly_made}, "
+            report += f"Net Profit/Loss: {net_profit}\n"
+            
+            self.stock += monthly_made - monthly_sold
+            monthly_sold = random.randint(5, 15)
+            monthly_made = monthly_sold + random.randint(-5, 5)
+            net_profit = (self.price - self.cost) * monthly_sold
+        
+        return report
